9658156dca76df9207c5278ad77301344111de60
Joel Joby <joeljobyp@gmail.com>
2025-10-21 13:46:32 +0530

    added few more fields

--- src/app/api/v1/visits/route.ts
+++ src/app/api/v1/visits/route.ts
@@ -1,12 +1,18 @@
 import { NextRequest, NextResponse} from "next/server";
-import { getClientIPv4 } from "@/lib/utils/user-client/getClientIP";
 import userClientRequestsController from "@/lib/controllers/client.requests.controller";
 
 export async function POST(req: NextRequest) {
     const userClientController = new userClientRequestsController(req);
-    const clientIp = await userClientController.getClientIP();
+    const ip_address = await userClientController.getClientIP();
+    const user_agent = await userClientController.getDeviceName();
+    
+    const refferer_url = req.headers.get("referer") || "";
+    const timestamp = new Date().toISOString();
     const device_information = await userClientController.getDeviceInfo();
-    console.log("Client IP :", clientIp);
+    const cookies = req.cookies;
+
+    
+    console.log("Client IP :", ip_address);
     console.log("Device Information :", device_information);
-    return NextResponse.json({ message: "Visit logged successfully", clientIp, device_information }, { status: 200 });
+    return NextResponse.json({ message: "Visit logged successfully", ip_address, device_information, user_agent, refferer_url, timestamp, cookies }, { status: 200 });
 }

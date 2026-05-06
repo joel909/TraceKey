3a210e7eae1b4d6d7107d4722ccfd8bfb8ad2271
Joel Joby <joeljobyp@gmail.com>
2026-05-06 14:07:55 +0530

    removed SSL from code since database is in docker instacne and ssl setup is not yet done

--- src/lib/database/config/db.ts
+++ src/lib/database/config/db.ts
@@ -6,10 +6,12 @@ import validateDatabaseResult from "./databaseResultValidator";
 
 dotenv.config();
 //connection pooling insted of creating a new connection for every query
+if (!process.env.SSLTRUE) {}
 export const pool = new Pool({
-    ssl: {
-      rejectUnauthorized: false, 
-    },
+  //If ur database is exposed over the internet, it's crucial to use SSL to encrypt the connection and protect sensitive data from being intercepted by malicious actors. However, if your database is hosted in a secure environment (like a private network or local development), you might choose to disable SSL for simplicity and performance reasons.
+    // ssl: {
+    //   rejectUnauthorized: false, 
+    // },
     connectionString: process.env.POSTGRES_URL,
 });
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
